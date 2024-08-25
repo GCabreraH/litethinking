5715d65cd5f547450f8adde328567224b2ba25b8
GCabreraH <guillermo.cabrera252@gmail.com>
2024-08-24 23:23:06 -0400

    Se elimina dependencia no usada

--- pageobjectmodel/src/test/java/saucedemo/features/LoginTest.java
+++ pageobjectmodel/src/test/java/saucedemo/features/LoginTest.java
@@ -6,16 +6,16 @@ import java.time.Duration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;//se debe usar junit.jupiter.api.xxx y no junit.jupiter.xxxxx
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.concurrent.TimeUnit;
-
 import saucedemo.pages.LoginPage;
 
 public class LoginTest {
     //declaracion de variables generales
 
     WebDriver driver;
+    int milli = 3000;
 
     @BeforeEach
     public void configDriver(){
@@ -33,18 +33,53 @@ public class LoginTest {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("standard_user", "secret_sauce");
         try {
-            Thread.sleep(10000);
+            Thread.sleep(milli);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
 }
-    @AfterEach
-    public void teardown(){
-        driver.close();
 
+@Test
+public void loginProblemaUser(){
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login("problem_user", "secret_sauce");
+    try {
+        Thread.sleep(milli);
+    } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
     }
 
+}@Test
+public void loginLockedUser(){
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login("locked_out_user", "secret_sauce");
+    try {
+        Thread.sleep(milli);
+    } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
 
+}
+
+@Test
+public void loginWrongUser(){
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login("dsnt_exist_user", "secret_sauce");
+    try {
+        Thread.sleep(milli);
+    } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+}
+    @AfterEach
+    public void teardown(TestInfo testInfo) { // Inyecta TestInfo en el método
+        driver.close();
+        System.out.println("El nombre del método es: " + testInfo.getDisplayName()); // Imprime el nombre del método
+    }
 }
